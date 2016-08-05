111142eee6fb8d28442e1a989cddf5772e1cb96e
Olev Abel <olev.abel@lab.mobi>
2016-08-05 09:29:52 +0300

    modified index file

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,9 @@
-import { bootstrap }    from 'angular2/platform/browser';
+
 
 // Our main component
 import { AppComponent } from './app.component';
 
 import 'rxjs/Rx';
+import {bootstrap} from "angular2/src/upgrade/angular_js";
 
 bootstrap(AppComponent);
